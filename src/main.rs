e9e58edb57ce997c3ccacc7efad9c444f89f4715
deimos <deimoshall@gmail.com>
2025-10-30 15:22:17 -0600

    feat: generate random password using random_str crate

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 use std::error::Error;
+use random_str as random;
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui: AppWindow = AppWindow::new()?;
     println!("Hello");
+
+    ui.on_request_generate_password({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            // TODO: add error handling
+            let random_password = random::get_string(
+                16,
+                ui.get_lowercase(),
+                ui.get_uppercase(),
+                ui.get_numbers(),
+                ui.get_symbols()
+            );
+
+            println!("Password: {}", random_password);
+        }
+    });
 
     ui.run()?;
 
