db0314e85dfe13539e41dd83b8ed629ae77b9f6b
deimos <deimoshall@gmail.com>
2025-10-30 14:44:02 -0600

    feat: add slint ui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui: AppWindow = AppWindow::new()?;
+    println!("Hello");
+
+    ui.run()?;
+
+    Ok(())
 }
